53fcc282252dbf5fed22c893b4d44ca117176dbc
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-16 15:29:37 +0100

    problem analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// ask a word to user with prompt
+//create a reversWord function with split and reverse
+//Create a if condition to check if the reverse word === original one. 
+
+
+// tools
+
+
+// prompt
+// funtion
+// if/else
+// console.log
+// /variables
+
+
+
+
+
+
+
+
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
